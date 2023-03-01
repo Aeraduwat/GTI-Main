11f0874da8856f31522ab5982be28f07b5b9a59b
Aeraduwat <felipemadrid95@gmail.com>
2023-03-01 19:40:23 -0400

    build 1

--- servidor/index.js
+++ servidor/index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const cors = require('cors');
 // const mongoose = require('mongoose');
 const path = require('path');
-const app= express();
+const app = express();
 
 const server = http.createServer(app)
 /*
@@ -17,28 +17,24 @@ if(procces.env.NODE_ENV == "production"){
     })
 }
 */
-connectarDB();
 app.use(cors());
 app.use(express.json());
 
 const _dirname = path.dirname("")
-const buildPath = path.join(_dirname, "")
+const buildPath = path.join(_dirname, "../cliente/src/app/")
 
 app.use(express.static(buildPath))
 
-//api
-app.use('/api/reuniones', require('./routes/reunion'));
-
-app.use(express.static(path.join("../cliente/build")))
-app.get("*",(req,res) => {
-    res.sendFile(path.resolve(buildPath,'../cliente', 'build', 'index.html'));
-})
-
 /*
+app.get("*",(req,res) => {
+    res.sendFile(path.resolve(buildPath,'../cliente', 'src', 'index.html'));
+})
+*/
+
 app.get("/*" , function(req, res){
 
     res.sendFile(
-        path.join(_dirname, "../cliente/dist/index.html"),
+        path.join(_dirname, "../cliente/src/app/app.component.html"),
         function(err){
             if(err){
                 res.status(500).send(err);
@@ -46,13 +42,19 @@ app.get("/*" , function(req, res){
         }
     );
 })
-*/
+
+
+//api
+app.use('/api/reuniones', require('./routes/reunion'));
+
+
 // //demo
 // app.get("/",(req,resp)=> {
 //     resp.send("app is working")
 // })
 
 
-app.listen(4000, () => console.log('Desde puerto 4000'));
+connectarDB();
+server.listen(4000, () => console.log('Desde puerto 4000'));
 
 console.log('desde app');

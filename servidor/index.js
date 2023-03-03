c3d86d96a6c5cea7ee6514157ef39df6ab6ec5aa
Aeraduwat <felipemadrid95@gmail.com>
2023-03-03 11:09:40 -0400

    carga 2

--- servidor/index.js
+++ servidor/index.js
@@ -8,40 +8,9 @@ const path = require('path');
 const app = express();
 
 const server = http.createServer(app)
-/*
-//Server production assets
-if(procces.env.NODE_ENV == "production"){
-    app.use(express.static(path.join("cliente/build")))
-    app.get("*",(req,res) => {
-        res.sendFile(path.resolve(__dirname,'cliente', 'dist', 'index.html'));
-    })
-}
-*/
+
 app.use(cors());
 app.use(express.json());
-
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname, "../cliente/src/app/")
-
-app.use(express.static(buildPath))
-
-/*
-app.get("*",(req,res) => {
-    res.sendFile(path.resolve(buildPath,'../cliente', 'src', 'index.html'));
-})
-*/
-
-app.get("/*" , function(req, res){
-
-    res.sendFile(
-        path.join(_dirname, "../cliente/src/app/app.component.html"),
-        function(err){
-            if(err){
-                res.status(500).send(err);
-            }
-        }
-    );
-})
 
 
 //api
@@ -58,3 +27,33 @@ connectarDB();
 server.listen(4000, () => console.log('Desde puerto 4000'));
 
 console.log('desde app');
+/*
+//Server production assets
+if(procces.env.NODE_ENV == "production"){
+    app.use(express.static(path.join("cliente/build")))
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,'cliente', 'dist', 'index.html'));
+    })
+}
+
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname, "../cliente/src/app/")
+
+app.use(express.static(buildPath))
+
+app.get("*",(req,res) => {
+    res.sendFile(path.resolve(buildPath,'../cliente', 'src', 'index.html'));
+})
+app.get("/*" , function(req, res){
+
+    res.sendFile(
+        path.join(_dirname, "../cliente/src/index.html"),
+        function(err){
+            if(err){
+                res.status(500).send(err);
+            }
+        }
+    );
+})
+*/
+
